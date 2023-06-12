3d0f8c77efb083eb16a6c669aa5465727ccc6938
Aaron Vontell <aaron@vontech.org>
2023-06-12 00:00:11 +0000

    Use list

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ export async function runTurn(bot: RGBot) {
     try {
         
         // Find the closest diamond and approach it
-        const diamondBlock = bot.findBlocks("diamond_block")
+        const diamondBlock = bot.findBlocks(["diamond_block"])
         bot.chat(JSON.stringify(diamondBlock))
         await bot.waitForMilliseconds(4000)
 

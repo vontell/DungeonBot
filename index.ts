16fd1dd4d1f343bd15db766c2d226c5b7f7ebeeb
Aaron Vontell <aaron@vontech.org>
2023-06-17 18:24:33 +0000

    Pickup

--- index.ts
+++ index.ts
@@ -44,13 +44,13 @@ export async function runTurn(bot: RGBot) {
     if (!move) return;
     try {
         // Find the closest diamond and approach it
-        const ids = [bot.mineflayer().registry.blocksByName["diamond_block"].id]
-        const diamondBlock = bot.mineflayer().findBlocks({ matching: ids, maxDistance: 32 })[0]
-        bot.chat(JSON.stringify(diamondBlock))
-        await bot.approachPosition(diamondBlock)
-        if (!diamondBlock) {
+        // const ids = [bot.mineflayer().registry.blocksByName["diamond_block"].id]
+        // const diamondBlock = bot.mineflayer().findBlocks({ matching: ids, maxDistance: 32 })[0]
+        // bot.chat(JSON.stringify(diamondBlock))
+        // await bot.approachPosition(diamondBlock)
+        // if (!diamondBlock) {
             await bot.findAndCollectItemsOnGround()
-        }
+        // }
     } catch(exception) {
         console.warn(exception)
     }
